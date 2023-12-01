14d29823af984cc1d0145481b848042deb2659f9
Georgy-Ribl <geokorneev2006@gmail.com>
2023-12-01 23:27:42 +0300

    add

--- other/defines.py
+++ other/defines.py
@@ -2,7 +2,7 @@ import random
 import array
 
 
-def creat_matrix(a: int, b: int) -> array:
+def creat_matrix(a: int, b: int) -> list:
     arr = [[0 for s in range(b)] for _ in range(a)]
     for i in range(a):
         for j in range(b):
--- recursionn/3.py
+++ recursionn/3.py
@@ -4,17 +4,25 @@ import sys
 
 def f(m, n):
     global arr
-    if m == 1 and n == 1:
-        return arr[1][1]
     if check[m][n] == 0:
-        return max(f(m - 1, n) + arr[m][n], f(m, n - 1) + arr[m][n])
-    else:
-        return
+        if m == 0 and n == 0:
+            check[m][n] = arr[0][0]
+        elif m == 0 and n != 0:
+            check[m][n] = arr[0][n]
+        elif n != 0 and n == 0:
+            check[m][n] = arr[m][0]
+        else:
+            if m > 1 and n > 1:
+                check[m][n] = max(f(m - 1, n) + arr[m][n], f(m, n - 1) + arr[m][n])
+            else:
+                if m == 0 and n != 0:
+                    check[0][n] = arr[0][n]
+                elif n != 0 and n == 0:
+                    check[m][0] = arr[m][0]
+        return check[m][n]
 
 
 m, n = map(int, input().split(" "))
 arr = defines.creat_matrix(100, 100)
 check = [[0 for _ in range(100)] for s in range(100)]
-# for elem in arr:
-#     print(*elem)
 print(f(m, n))
